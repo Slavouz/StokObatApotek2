7e58a23a5db8375988df7d8a3b0ba0779f86e513
Slavouz <tmbwpva@protonmail.com>
2024-11-06 11:43:28 +0700

    Dynamic Url

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,31 @@
+package koneksi;
+
+/**
+ *
+ * @author davinky
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+
+    private static Connection connect;
+
+    public static Connection koneksiDB() throws SQLException {
+        if (connect == null) {
+            try {
+                String projectDir = System.getProperty("user.dir");
+                String url = "jdbc:sqlite:" + projectDir + "/src/db/apotek.db";
+                connect = DriverManager.getConnection(url);
+                System.out.println("Koneksi sukses");
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Koneksi gagal, menutup aplikasi...");
+                System.out.println(e);
+                System.exit(0);
+            }
+        }
+        return connect;
+    }
+}
